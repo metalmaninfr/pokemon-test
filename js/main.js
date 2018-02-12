ad218ad3557040c3cfbed01f64e65f74a525137d
metalmaninfr <alex_blandford@hotmail.com>
2018-02-12 01:23:31 +0100

    combat moves updated and working javaScript strange bug to correct: pressing down one more time on tail whip makes combatmemo = 1

--- js/main.js
+++ js/main.js
@@ -3,39 +3,48 @@ var fight = document.querySelector('.combat-fight-container');
 var arrow = document.querySelector('.arrow');
 var combatArrow = document.querySelector('.combat-arrow');
 var memo = 0
+var combatmemo = 0
 
 
 window.addEventListener('keyup', function(event){
   if (event.keyCode === 40) {
     arrow.style.top = "10rem";
-    combatArrow.style.top = "10rem";
+    combatArrow.style.top = "4.9rem";
+    combatmemo++
     memo = 1;
   }
+  if (event.keyCode === 40 && combatmemo === 2) {
+    combatArrow.style.top = "8rem";
+  }
+  if (event.keyCode === 40 && combatmemo === 3) {
+    combatArrow.style.top = "11.2rem";
+  }
+
   if (event.keyCode === 39 && memo === 0) {
     arrow.style.left = "12rem";
-    combatArrow.style.left = "10rem";
   }
 
   if (event.keyCode === 39 && memo === 1) {
     arrow.style.left = "14rem";
-    combatArrow.style.left = "12.5rem";
   }
 
   if (event.keyCode === 38) {
     arrow.style.top = "";
-    combatArrow.style.top = "";
-    memo = 0
+    memo = 0;
   }
   if (event.keyCode === 37) {
     arrow.style.left = "";
-    combatArrow.style.left = "";
   }
   if (arrow.style.top === "" && arrow.style.left === "" && event.keyCode === 13) {
     menu.style.display = "none";
-    fight.style.display = "flex";
+    fight.style.display = "block";
+    combatArrow.style.top = "";
+    combatmemo = 0;
   }
   if (event.keyCode === 8) {
     fight.style.display = "";
     menu.style.display = "";
+    arrow.style.top = "";
+    memo = 0;
   }
 });
